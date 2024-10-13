012dd05594c0acd40393601352083422836f316d
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-10-13 21:31:03 +0500

    refactor: delete users from the users array

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,17 +1,4 @@
 import { v4 as uuidv4 } from 'uuid'
 import { User } from '../shared/userInterface'
 
-export const users: User[] = [
-  {
-    id: uuidv4(),
-    username: 'John Doe',
-    age: 30,
-    hobbies: ['reading', 'coding']
-  },
-  {
-    id: uuidv4(),
-    username: 'Jane Smith',
-    age: 25,
-    hobbies: ['painting', 'dancing']
-  }
-]
+export const users: User[] = []
